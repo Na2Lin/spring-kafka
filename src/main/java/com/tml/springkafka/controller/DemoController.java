fbb29750437fc2644335506bb09e58fbb23ceba2
tianmaolin <tmaolin415@163.com>
2018-01-15 18:09:27 +0800

    1.

--- src/main/java/com/tml/springkafka/controller/DemoController.java
+++ src/main/java/com/tml/springkafka/controller/DemoController.java
@@ -6,9 +6,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -19,10 +19,8 @@ public class DemoController {
     KafkaTemplate<String,String> kafkaTemplate;
 
     @RequestMapping("send")
-    public String demoTest() {
-        Map message = new HashMap();
-        message.put("aaa","bbb");
-        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("topic_1", message.toString());
+    public String demoTest(@RequestParam Map map) {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("topic_1", map.toString());
         System.out.println(future);
         return "success";
     }
